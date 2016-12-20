5c41df93295dae630800aaf9785f86d99c1f1a91
Brent Perry <burentoperii@gmail.com>
2016-12-20 15:37:55 -0600

    return correct number, by adding more entries to map.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,17 @@ var output = "";
 
 var numberMap = {
   1000: 'M',
+  900: 'CM',
   500: 'D',
+  400: 'CD',
   100: 'C',
+  90: 'XC',
   50: 'L',
+  40: 'XL',
   10: 'X',
+  9: 'IX',
   5: 'V',
+  4: 'IV',
   1: 'I'
 };
 
@@ -26,12 +32,11 @@ var convertNumber = function(number) {
     output += numberMap[workingNumber];
   } else if (workingNumber > 4) {
     var large = findBiggest(workingNumber);
+    console.log(large);
     var icount = workingNumber % large;
     if (icount > 3) {
       output += numberMap[large];
-      console.log(output);
-      workingNumber = workingNumber % large;
-      console.log(workingNumber);
+      workingNumber = workingNumber - large;
       convertNumber(workingNumber);
     } else {
       output += numberMap[large] + "I".repeat(workingNumber % large);
